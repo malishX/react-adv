89b7b1d5a80f44ed5859710e05f5e95ffbf08b87
malishX <hschool510@gmail.com>
2021-10-17 00:20:42 +0400

    change 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-
-import Setup from './tutorial/1-useState/setup/3-useState-array'
-
+import React from "react";
+import Final from "./tutorial/1-useState/final/3-useState-array";
+//import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 
 function App() {
   return (
-    <div className='container'>
-      <Setup/>
-   
+    <div className="container">
+      <Final />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data)
+  const [people, setPeople] = React.useState(data);
   const removeItem = (id) => {
-    let newPeople = people.filter(
-      (person)=> person.id !== id
-    )
-    setPeople(newPeople);
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople((oldPeople) => {
+      let newPeople = oldPeople.filter((person) => person.id !== id);
+      return newPeople;
+    });
   };
-  return <>
-  {
-   people.map(
-     (person) => {
-       const {id, name} = person;
-       return (
-       <div key={id} className='item'>
-          <h4>{name}</h4>
-          <button className='btn' onClick={()=>removeItem(id)}>remove</button>
-       </div>
-       )
-     }
-   )
-  }
-  <button className='btn' onClick={()=>setPeople([])}>
-
-    clear item
-  </button>
-  </>;
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear item
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
